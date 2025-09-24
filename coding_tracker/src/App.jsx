3264faa015dcbb886868c2d432f72ecdd6b99eba
r sai siddhu <rsaisiddhu@gmail.com>
2025-09-24 13:10:38 +0530

    created public and index.html

--- coding_tracker/src/App.jsx
+++ coding_tracker/src/App.jsx
@@ -1,5 +1,5 @@
-// src/App.jsx
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// App.jsx
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ForgotPassword from "./pages/ForgotPassword";
@@ -9,11 +9,15 @@ function App() {
   return (
     <Router>
       <Routes>
+        {/* Public Auth Routes */}
         <Route element={<AuthLayout />}>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
         </Route>
+
+        {/* Private Routes Example (Dashboard etc) */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
   );
--- coding_tracker/src/main.jsx
+++ coding_tracker/src/main.jsx
@@ -1,8 +1,6 @@
-// src/main.jsx (Vite)
-// OR src/index.js (CRA)
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App"; // or "./pages/App" if your App.jsx is inside src/pages
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
