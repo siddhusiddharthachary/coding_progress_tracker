6cefcfce47e447f1fc137656719f2cafd38446d7
r sai siddhu <rsaisiddhu@gmail.com>
2025-09-24 12:44:30 +0530

    added App.jsx and main.jsx

---
+++ coding_tracker/src/App.jsx
@@ -0,0 +1,22 @@
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ForgotPassword from "./pages/ForgotPassword";
+import AuthLayout from "./layouts/AuthLayout";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ coding_tracker/src/main.jsx
@@ -0,0 +1,11 @@
+// src/main.jsx (Vite)
+// OR src/index.js (CRA)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
